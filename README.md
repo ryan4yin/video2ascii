a90f72a5510ac51cadd3a5649b328cfa5b39d78d
ryan <xiaoyin_c@qq.com>
2019-10-27 19:16:08 +0800

    update project info

--- README.md
+++ README.md
@@ -7,10 +7,11 @@ other versions:
 - [video2chars(python)](https://github.com/ryan4yin/video2chars)
 - [Video2ASCII.jl(julia)](https://github.com/ryan4yin/Video2ASCII.jl)
 
-
 >P.S. Video2ASCII is my personal "hello world" project, 
 every time I learn a new programming language, I will implement it in that languange.
 
+
 ## Reference
 
-The project structure refers to  [golang-standards/project-layout](https://github.com/golang-standards/project-layout)
+- [golang-standards/project-layout](https://github.com/golang-standards/project-layout)
+- [asciiplayer](https://github.com/qeesung/asciiplayer)
---
+++ cmd/video2ascii/constant.go
@@ -0,0 +1,24 @@
+package main
+
+const (
+	// Version version
+	Version = "0.0.1"
+	// HomePage homepage
+	HomePage = "https://github.com/ryan4yin/video2ascii"
+	// Author author
+	Author = "ryan4yin"
+	// ShortTitle shortTitle
+	ShortTitle = "video2ascii/" + Version + " _(>.ო.<)_ _(>.ო.<)_ _(>.ო.<)_"
+
+	// Title title
+	Title = `
+	             __                    ___                                       
+          __    /\ \                 /'___'\                           __  __    
+  __  __ /\_\   \_\ \     __    ___ /\_\ /\ \     __      ____    ___ /\_\/\_\   
+ /\ \/\ \\/\ \  /'_' \  /'__'\ / __'\/_/// /__  /'__'\   /',__\  /'___\/\ \/\ \  
+ \ \ \_/ |\ \ \/\ \L\ \/\  __//\ \L\ \ // /_\ \/\ \L\.\_/\__, '\/\ \__/\ \ \ \ \ 
+  \ \___/  \ \_\ \___,_\ \____\ \____//\______/\ \__/.\_\/\____/\ \____\\ \_\ \_\
+   \/__/    \/_/\/__,_ /\/____/\/___/ \/_____/  \/__/\/_/\/___/  \/____/ \/_/\/_/`
+	// SummaryTitle summary title
+	SummaryTitle = Title + "\n>>>Version  : " + Version + "\n>>>Author   : " + Author + "\n>>>HomePage : " + HomePage
+)
